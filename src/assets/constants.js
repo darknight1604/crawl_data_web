553c47f64d9dc332f93709c663eecdfe113d4709
Quoc Tan <darknight1604@gmail.com>
2021-10-22 14:18:44 +0700

    Add constants to manage api url

---
+++ src/assets/constants.js
@@ -0,0 +1,3 @@
+
+export const APIURL = "https://d980-42-114-225-40.ngrok.io/"
+// export const APIURL = "http://localhost:4001/"
--- src/services/crawlData.ts
+++ src/services/crawlData.ts
@@ -1,9 +1,10 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+import { APIURL } from '../assets/constants';
 import {Product} from '../types/product';
 
 export const crawlData = createApi({
     reducerPath: 'crawlData',
-    baseQuery: fetchBaseQuery({baseUrl: 'https://d980-42-114-225-40.ngrok.io/api/'}),
+    baseQuery: fetchBaseQuery({baseUrl: APIURL + 'api/'}),
     endpoints: (builder) => ({
         getDataCollection: builder.query<Array<Product>, void>({
             query: () => `crawl-data/0`,
