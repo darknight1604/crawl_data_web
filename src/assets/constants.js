6fde21be8c854dfb9852d469e9100f0a92b0f605
Quoc Tan <darknight1604@gmail.com>
2021-10-23 10:49:41 +0700

    Update api

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,3 +1,3 @@
 
-export const APIURL = "https://d980-42-114-225-40.ngrok.io/"
-// export const APIURL = "http://localhost:4001/"
+// export const APIURL = "http://1e5f-1-53-26-104.ngrok.io/"
+export const APIURL = "http://localhost:4001/"
