3fe447b1f0206a99758c3610928e26bdaf83b749
Quoc Tan <darknight1604@gmail.com>
2021-10-24 22:27:38 +0700

    Add new constant

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,3 +1,5 @@
 
 export const APIURL = "http://1e5f-1-53-26-104.ngrok.io/"
 // export const APIURL = "http://localhost:4001/"
+
+export const ERROR_MESSAGE = "Ops, something went wrong please try again"
