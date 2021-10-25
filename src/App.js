0909a5be685bce8099b35b0e037b4b103aa2cb9b
Quoc Tan <darknight1604@gmail.com>
2021-10-25 16:05:06 +0700

    Add theme of header title

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { useGetDataCollectionQuery, } from './services/crawlData';
 import ShopeeCollection from './components/ShopeeCollection';
 import ShopeeSearchCollection from './components/ShopeeSearchCollection';
 import ShopeeSoldOutCollection from './components/ShopeeSoldOutCollection';
-import { Grid } from '@mui/material';
+import { createTheme, Grid, responsiveFontSizes } from '@mui/material';
 import ErrorData from './components/ErrorData';
+
+let theme = createTheme();
+
+theme = responsiveFontSizes(theme);
 
 function App() {
   const { data, error, isLoading } = useGetDataCollectionQuery();
@@ -29,9 +33,9 @@ function App() {
               <div></div>
           </Grid>
           <Grid item xs={6} md={12} sm={12} lg={8}> 
-            <ShopeeCollection products={data}/>
-            <ShopeeSearchCollection />
-            <ShopeeSoldOutCollection />
+            <ShopeeCollection products={data} titleTheme={theme}/>
+            <ShopeeSearchCollection titleTheme={theme}/>
+            <ShopeeSoldOutCollection titleTheme={theme}/>
           </Grid>
           <Grid item lg={2} xs={3}>
               <div></div>
