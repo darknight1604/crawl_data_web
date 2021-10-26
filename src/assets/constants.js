194e8bada28730c019dc79bf42459b191c2b569c
Quoc Tan <darknight1604@gmail.com>
2021-10-26 07:46:14 +0700

    Update api

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,5 +1,5 @@
 
-export const APIURL = "http://9fbf-1-53-222-92.ngrok.io/"
+export const APIURL = "http://5137-1-53-222-92.ngrok.io/"
 // export const APIURL = "http://localhost:4001/"
 
 export const ERROR_MESSAGE = "Ops, something went wrong please try again"
